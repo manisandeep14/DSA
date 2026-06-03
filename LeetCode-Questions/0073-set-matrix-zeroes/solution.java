7bad9ce3b6581ca209ee773851f65cf0b90cfddb
Mani Sandeep <manisandeepms32m16@gmail.com>
2026-06-03 16:04:53 +0000

    [LeetCode Sync] Runtime - 1 ms (95.96%), Memory - 47.5 MB (60.98%)

--- LeetCode-Questions/0073-set-matrix-zeroes/solution.java
+++ LeetCode-Questions/0073-set-matrix-zeroes/solution.java
@@ -1,56 +1,79 @@
 public class Solution {
 
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+
+        //Brute Force approach
         
-        int r[] = new int[n];
-        int c[] = new int[m];
+        // int n = matrix.length;
+        // int m = matrix[0].length;
         
-        int rInd = 0;
-        int cInd = 0;
+        // int r[] = new int[n];
+        // int c[] = new int[m];
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        // int rInd = 0;
+        // int cInd = 0;
+        
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         if(matrix[i][j] == 0){
+                    
+        //             boolean rowExists = false;
+        //             for(int k = 0; k < rInd; k++) {
+        //                 if(r[k] == i) rowExists = true;
+        //             }
+        //             if(!rowExists && rInd < n){
+        //                 r[rInd] = i;
+        //                 rInd++;
+        //             }
+        //             boolean colExists = false;
+        //             for(int k = 0; k < cInd; k++) {
+        //                 if(c[k] == j) colExists = true;
+        //             }
+        //             if(!colExists && cInd < m){
+        //                 c[cInd] = j; 
+        //                 cInd++;
+        //             }
+        //         }
+        //     }
+        // }
+        
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         for(int k = 0; k < rInd; k++){
+        //             if(r[k] == i){
+        //                 matrix[i][j] = 0;
+        //             }
+        //         }
+        //         for(int k = 0; k < cInd; k++){
+        //             if(c[k] == j){
+        //                 matrix[i][j] = 0;
+        //             }
+        //         }
+        //     }
+        // }
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        boolean[] r = new boolean[row];
+        boolean [] c = new boolean[col];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col ; j++){
                 if(matrix[i][j] == 0){
-                    
-                    // 1. Only add row i if it hasn't been recorded yet
-                    boolean rowExists = false;
-                    for(int k = 0; k < rInd; k++) {
-                        if(r[k] == i) rowExists = true;
-                    }
-                    if(!rowExists && rInd < n){
-                        r[rInd] = i;
-                        rInd++;
-                    }
-                    
-                    // 2. Only add column j if it hasn't been recorded yet
-                    boolean colExists = false;
-                    for(int k = 0; k < cInd; k++) {
-                        if(c[k] == j) colExists = true;
-                    }
-                    if(!colExists && cInd < m){
-                        c[cInd] = j; // Storing the column index 'j'
-                        cInd++;
-                    }
+                    r[i] = true;
+                    c[j] = true;
                 }
             }
         }
-        
-        // Your exact logic to zero out the matrix rows and columns
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                for(int k = 0; k < rInd; k++){
-                    if(r[k] == i){
-                        matrix[i][j] = 0;
-                    }
-                }
-                for(int k = 0; k < cInd; k++){
-                    if(c[k] == j){
-                        matrix[i][j] = 0;
-                    }
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(r[i] == true || c[j] == true){
+                    matrix[i][j] = 0;
                 }
             }
         }
+
     }
 }
